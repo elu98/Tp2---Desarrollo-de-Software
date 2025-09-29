74296a1ab2b7e76a270b4242b5788bc3189ace60
Ivan <B_I_T_T@outlook.com>
2025-09-29 22:16:46 +0000

    Agrego el script de bash para crear el ambiuente virtual usando pipenv.

---
+++ EjPractico2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if python3 --version &> /dev/null; then
+    echo "Python 3 is installed."
+else
+    echo "Python 3 is not installed. Please install Python 3 to proceed."
+    sudo apt install python3
+fi
+
+if pip3 --version &> /dev/null; then
+    echo "pip3 is installed."
+else
+    echo "pip3 is not installed. Installing pip3..."
+    sudo apt install python3-pip
+fi
+
+mkdir -p {static/{css,images},templates,.venv}
+touch app.py
+
+pipenv install Flask
+pipenv install Flask-Mail
+
+echo "Virtual Environment created and Flask installed."
+
